a5094eeaf6446904d09238443352e4390f2ba4b1
Oleksandr Hrytsenko <oleksandrhry@wizardsdev.com>
2020-05-31 13:55:44 +0300

    create-simple-react-custom-hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React from 'react';
+    import React from 'react';
 import './index.css';
 import { hot } from "react-hot-loader";
 import Counter from './components/Counter.jsx'
-import Mobile from "./components/touchEvents/Mobile";
-import EventComponent from "./components/touchEvents/EventComponent";
 
 const App = () => (
     <div>
         <Counter/>
-        <Mobile/>
-        <EventComponent/>
     </div>
 );
 
